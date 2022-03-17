a976e4192fce80497896d3d7e3f9a9ccd1373b98
林承美 <91170746+choww26@users.noreply.github.com>
2022-03-17 17:38:11 +0800

    Add files via upload

---
+++ week04-4.cpp
@@ -0,0 +1,14 @@
+///struct結構(方便排序)
+#include <stdio.h>
+struct DATA{
+    char c;
+    int ans;
+};///發明一種有字母和ans出現次數的資料結構
+
+struct DATA list1;///一個資料結構的東西
+struct DATA lists[100];///100個資料結構的東西
+int main()
+{
+    list1.c='A';
+    list1.ans=1;
+}
